96343cc1694431938fb689220770eec33cfaf3bd
Yunus ÖLMEZ <yns4832@gmail.com>
2022-10-08 19:05:54 +0200

    A simple design and scss files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <Header />
+
+  <div class="container">
+    <router-view />
+  </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Header from "@/components/Header.vue";
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@Options({
+  components: {
+    Header,
+  },
+})
+export default class App extends Vue {}
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>
+            <router-link to="/" class="home-link">ABN TV MAZE</router-link>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class HomeView extends Vue {}
+</script>
+<style lang="scss" scoped>
+.header {
+  background: $green-gradient;
+  padding: $spacing-4;
+
+  a.home-link {
+    color: $white;
+    font-family: $title-font;
+  }
+}
+</style>
