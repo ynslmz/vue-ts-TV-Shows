bc593dd4d08aa8ca457d7620dfe31829975df1bf
Yunus ÖLMEZ <yns4832@gmail.com>
2022-10-20 23:52:14 +0200

    remove unused line

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,5 @@ import "@/assets/scss/styles.scss";
 
 import { ShowStore } from "./store/show.module";
 
-/// Dependency injection Setup
 store.dispatch(ShowStore.LOAD_SHOWS);
-
-createApp(App)
-  .provide("apiService", "ApiService")
-  .use(store)
-  .use(router)
-  .mount("#app");
+createApp(App).use(store).use(router).mount("#app");
